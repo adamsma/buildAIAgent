588041928af9c5628acde4c5c8fcc69321f6b3ae
Marcus Adams <8154210+adamsma@users.noreply.github.com>
2025-11-29 20:57:49 -0500

    add prompt and verbose command line arguments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -9,13 +10,30 @@ api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
 def main():
+
+    if(len(sys.argv) < 2):
+        print("Provide a question or prompt as a command line argument")
+        sys.exit(1)
+
+    user_prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text = user_prompt)]),
+    ]
+
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', 
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        model = 'gemini-2.0-flash-001', 
+        contents = messages,
     )
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
     print(response.text)
-    print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-    print("Response tokens:", response.usage_metadata.candidates_token_count)
+
+    if("--verbose" in sys.argv):
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 
 if __name__ == "__main__":
