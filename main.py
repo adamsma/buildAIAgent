c02bb30809aafe273e0d561015e954d64443ef3b
Marcus Adams <8154210+adamsma@users.noreply.github.com>
2025-12-02 20:44:08 -0500

    updated model and used argparse instead of manual parsing

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 import os
 import sys
+import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-client = genai.Client(api_key=api_key)
-
 def main():
 
-    if(len(sys.argv) < 2):
-        print("Provide a question or prompt as a command line argument")
-        sys.exit(1)
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
-    user_prompt = sys.argv[1]
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY environment variable not set")
 
     messages = [
-        types.Content(role="user", parts=[types.Part(text = user_prompt)]),
+        types.Content(role="user", parts=[types.Part(text = args.prompt)]),
     ]
 
+    client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model = 'gemini-2.0-flash-001', 
+        model = 'gemini-2.5-flash', 
         contents = messages,
     )
+
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API response appears to be malformed")
+    
     prompt_tokens = response.usage_metadata.prompt_token_count
     response_tokens = response.usage_metadata.candidates_token_count
 
     print(response.text)
 
     if("--verbose" in sys.argv):
-        print(f"User prompt: {user_prompt}")
+        print(f"User prompt: {args.prompt}")
         print(f"Prompt tokens: {prompt_tokens}")
         print(f"Response tokens: {response_tokens}")
 
