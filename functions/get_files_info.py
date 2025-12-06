72b45e9403f835bc7d4e88b6c63556d3af66fb70
Marcus Adams <8154210+adamsma@users.noreply.github.com>
2025-12-05 20:08:27 -0500

    add calculator app

---
+++ functions/get_files_info.py
@@ -0,0 +1,15 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+
+    full_path = os.path.join(working_directory, directory)
+    full_path = os.path.abspath(full_path)
+
+    if(not full_path.startswith(working_directory)):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    
+    if(not os.path.isdir(full_path)):
+        return f'Error: "{directory}" is not a directory'
+    
+
+    pass
