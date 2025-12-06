0642321441f14cd40501840bd3e1054ddb98c0de
Marcus Adams <8154210+adamsma@users.noreply.github.com>
2025-12-05 21:04:55 -0500

    add get file info function with tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -2,14 +2,21 @@ import os
 
 def get_files_info(working_directory, directory="."):
 
-    full_path = os.path.join(working_directory, directory)
-    full_path = os.path.abspath(full_path)
-
-    if(not full_path.startswith(working_directory)):
-        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    try:
+        full_path = os.path.join(working_directory, directory)
+        full_path = os.path.abspath(full_path)
+        
+        if(not full_path.startswith(os.path.abspath(working_directory))):
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        
+        if(not os.path.isdir(full_path)):
+            return f'Error: "{directory}" is not a directory'
+        
+        files = [os.path.join(full_path, f) for f in os.listdir(full_path)]
+        lines = [f'- {os.path.basename(f)}: file_size={os.path.getsize(f)} bytes, is_dir={os.path.isdir(f)}' for f in files]
+        lines = [f"Result for {"current" if directory == "." else "'" + directory + "'"} directory:"] + lines
     
-    if(not os.path.isdir(full_path)):
-        return f'Error: "{directory}" is not a directory'
+    except Exception as e:
+        return f'Error: {e}'
     
-
-    pass
+    return "\n".join(lines)
