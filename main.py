13e0bd9232a85420a17cc0450614d299e3cb7c51
Marcus Adams <8154210+adamsma@users.noreply.github.com>
2025-12-08 21:04:39 -0500

    add system prompt

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import *
 
 def main():
 
@@ -25,6 +26,7 @@ def main():
     response = client.models.generate_content(
         model = 'gemini-2.5-flash', 
         contents = messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt),
     )
 
     if not response.usage_metadata:
